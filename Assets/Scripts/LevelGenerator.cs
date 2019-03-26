5d73f517445cd0ff88c98951d2e4f8cc56405bd3
ppRadu <32032445+ppRadu@users.noreply.github.com>
2019-03-26 19:44:29 +0200

    v0.1 - Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+
+    public GameObject platformPrefab;
+    public GameObject highPlatformPrefab;
+
+    public int numberOfPlatforms;
+    public float levelWidth = 3f;
+    public float minY = 3f;
+    public float maxY = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            spawnPosition.y += Random.Range(minY, maxY);
+
+            if (i % 100 == 0)
+            {
+                Instantiate(highPlatformPrefab, spawnPosition, Quaternion.identity);  
+            }
+            else
+            {
+                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            }
+        }
+
+	}
+
+
+}
